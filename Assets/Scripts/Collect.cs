df99b5d6f4b03c417bb12e57e1f2157f2971accb
jakeb4 <39779464+jakeb4@users.noreply.github.com>
2018-07-12 15:25:45 -0500

    yo! Collect those keys broooo
    yo! Collect those keys broooo

---
+++ Assets/Scripts/Collect.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Collect : MonoBehaviour {
+
+    public int keyCount;
+
+    [SerializeField]
+    private int MaxAmountOfKeys = 3;
+
+    public Text KeyCount;
+
+    private void Start()
+    {
+        
+        keyCount = 0;
+        MaxAmountOfKeys = 3;
+        KeyCount.text = keyCount.ToString();
+    }
+
+    private void OnTriggerEnter(Collider col)
+    {
+        if(col.gameObject.tag == "Key")
+        {
+            if (keyCount < MaxAmountOfKeys)
+            {
+                col.gameObject.SetActive(false);
+                keyCount++;
+                KeyCount.text = keyCount.ToString();
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Collectable.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collectable : Collection {
+
+    public override void Collect()
+    {
+        base.Collect();
+    }
+
+    private void OnTriggerEnter(Collider col)
+    {
+        if(col.gameObject.tag == "Player")
+        {
+            Collect();
+        }
+    }
+}
